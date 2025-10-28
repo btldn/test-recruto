115654cb93e40a5ef3d7ca57ff22cb369d416e32
btldn <darkzell20@gmail.com>
2025-10-29 01:30:41 +0300

    feat: ready to push test task

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    name = request.args.get("name", "Recruto").strip()
+    message = request.args.get("message", "Давай дружить").strip()
+    text = f"Hello {name}! {message}!"
+    return Response(text, mimetype="text/plain; charset=utf-8")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
